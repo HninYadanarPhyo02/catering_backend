d2098bc72a059b2d3333b8e0de8391e8f84545e7
HninYadanarPhyo02 <hninyadanarphyo38380@gmail.com>
2025-08-27 10:51:26 +0630

    Updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,6 +79,7 @@
             </div>
             <div class="d-grid gap-2">
                 <button type="submit" class="btn btn-primary" style="background-color : white; color: black; border: 2px solid white;">Login</button>
+
             </div>
          </form> 
     
